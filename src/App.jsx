aecf260ebeea37b3d439c3ed03f62d45043dfbae
OksanaFedotova <oxana-fedotova@yandex.ru>
2024-06-19 16:41:35 +0400

    fix: remove neon coursor

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,16 @@ import './App.css';
 import Works from './routes/Works/Works';
 import Blog from './routes/Blog/Blog';
 import PostRoute from './routes/Blog/PostRoute';
-import { useEffect } from 'react';
-import { neonCursor } from "https://unpkg.com/threejs-toys@0.0.8/build/threejs-toys.module.cdn.min.js";
+
 
 function App() {
-  useEffect(() => {
-    neonCursor({
-      el: document.getElementById("root"),
-      shaderPoints: 16,
-      curvePoints: 80,
-      curveLerp: 0.5,
-      radius1: 5,
-      radius2: 30,
-      velocityTreshold: 10,
-      sleepRadiusX: 100,
-      sleepRadiusY: 100,
-      sleepTimeCoefX: 0.0025,
-      sleepTimeCoefY: 0.0025,
-    });
-  }, []);
   return (
-    <>sdfs</>
-    // <Routes>
-    //   <Route path="/" element={<Main />} />
-    //   <Route path="/works" element={<Works />} />
-    //   <Route path="/blog" element={<Blog />} />
-    //   <Route path="/blog/:id" element={<PostRoute/>} />
-    // </Routes>
+    <Routes>
+      <Route path="/" element={<Main />} />
+      <Route path="/works" element={<Works />} />
+      <Route path="/blog" element={<Blog />} />
+      <Route path="/blog/:id" element={<PostRoute/>} />
+    </Routes>
   );
 }
 
