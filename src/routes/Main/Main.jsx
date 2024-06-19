6f38bba1304a28ff21909bf4bb3888de1a3da6a5
Aleksandr Varvarin <varv.17@mail.ru>
2024-06-19 16:57:00 +0300

    topic2

--- src/routes/Main/Main.jsx
+++ src/routes/Main/Main.jsx
@@ -13,12 +13,11 @@ export default function Main() {
         <div className="main-container-info">
           <span className="main-rectangle" />
           <div className="h1-container">
-            <h1 className="main-h1">Меня зовут Оксана,</h1>
-            <h1 className="main-h1"> я веб разработчик</h1>
+            <h1 className="main-h1">Привет! Меня зовут Александр Варварин,</h1>
+            <h1 className="main-h1"> я веб-дизайн</h1>
           </div>
           <p className="main-p">
-            Я создаю высокопроизводительные, красивые веб-сайты, которые ориентированы на конверсию,
-            соответствуют бренду и удобны для людей.
+            Я создаю макет сайта.
           </p>
           <Button
             className={`button-form`}
